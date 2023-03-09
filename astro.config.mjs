6dd0b65cf66462e7d4b9c2a2214e5117b3b46a28
David Abell <79927957+david-abell@users.noreply.github.com>
2023-03-09 14:50:19 +0000

    update site meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [compress(), mdx(), tailwind()],
+  site: 'https://github.com/david-abell/',
 });
