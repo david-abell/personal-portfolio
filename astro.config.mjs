1cc425f6ec739083bbf46a1bfe660e746032d91d
David Abell <79927957+david-abell@users.noreply.github.com>
2023-03-09 15:37:22 +0000

    update base url for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import compress from 'astro-compress';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
+export const BASE_URL = '/personal-portfolio';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [compress(), mdx(), tailwind()],
   site: 'https://github.com/david-abell/',
+  base: BASE_URL,
 });
