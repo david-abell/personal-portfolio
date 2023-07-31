48b05d9592f9090c67b146219b5aeb999ea5c7d2
David Abell <79927957+david-abell@users.noreply.github.com>
2023-07-31 16:37:55 +0100

    remove astro/mdx update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 export const BASE_URL = '/personal-portfolio';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
+  integrations: [compress(), tailwind()],
   site: 'https://david-abell.github.io',
   base: BASE_URL,
 });
