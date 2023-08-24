1fefdc87d675b582c663ff567f0f9bc0cba10726
David Abell <david.abell.m@gmail.com>
2023-08-24 16:52:10 +0100

    fix: new eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,32 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
     es2022: true,
     browser: true,
     'astro/astro': true,
   },
-  extends: ['eslint:recommended', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-strict'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.eslint.json',
-    extraFileExtensions: ['.astro'],
-  },
-  rules: {},
+  extends: [
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-strict',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
   overrides: [
     {
-      files: ['*.js'],
-      rules: {
-        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-      },
-    },
-    {
+      // Define the configuration for `.astro` file.
       files: ['*.astro'],
+      // Allows Astro components to be parsed.
       parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
       parserOptions: {
         parser: '@typescript-eslint/parser',
+        // project: './tsconfig.eslint.json',
         extraFileExtensions: ['.astro'],
       },
-      rules: {
-        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-      },
     },
-    {
-      files: ['*.ts'],
-      parser: '@typescript-eslint/parser',
-      extends: ['plugin:@typescript-eslint/recommended'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
-        ],
-        '@typescript-eslint/no-non-null-assertion': 'off',
-      },
-    },
-    {
-      files: ['**/*.astro/*.js', '*.astro/*.js'],
-      parser: '@typescript-eslint/parser',
-    },
-  ],
+  ]
 };
