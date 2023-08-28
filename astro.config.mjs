4a473c5f348e436f72353e12862a0a53af99a2ea
David Abell <david.abell.m@gmail.com>
2023-08-28 17:32:20 +0100

    feat: auto create markdown toc, render emoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
+import emoji from 'remark-emoji';
+import rehypeToc from 'rehype-toc';
+import rehypeSlug from 'rehype-slug';
 
 export const BASE_URL = '/personal-portfolio';
 
@@ -9,4 +12,21 @@ export default defineConfig({
   integrations: [compress(), tailwind()],
   site: 'https://david-abell.github.io',
   base: BASE_URL,
+  markdown: {
+    remarkPlugins: [[emoji, { accessible: true }]],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeToc,
+        {
+          headings: ['h2', 'h3'],
+          nav: true,
+          cssClasses: {
+            toc: 'not-prose toc', // Change the CSS class for the TOC
+            link: 'page-link', // Change the CSS class for links in the TOC
+          },
+        },
+      ],
+    ],
+  },
 });
