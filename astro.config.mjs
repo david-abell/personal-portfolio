f6379c27fdc47aceb9822917e24b07a7f3e1b44c
David Abell <david.abell.m@gmail.com>
2023-09-15 08:59:19 +0100

    feat: add remark reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import emoji from 'remark-emoji';
+import { remarkReadingTime } from './src/lib/plugins/remark-reading-time.mjs';
 // import rehypeToc from 'rehype-toc';
 // import rehypeSlug from 'rehype-slug';
 
@@ -12,7 +13,7 @@ export default defineConfig({
   site: 'https://david-abell.github.io',
   base: BASE_URL,
   markdown: {
-    remarkPlugins: [[emoji, { accessible: true }]],
+    remarkPlugins: [[emoji, { accessible: true }], remarkReadingTime],
     // rehypePlugins: [
     //   rehypeSlug,
     //   [
